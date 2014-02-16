6471168f0a854639f9883dc648865b42ab30322e
sophia <scastellarin95@gmail.co>
2014-02-16 10:38:53 -0500

    actually started making an app

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
 //Define an angular module for our app
 var sampleApp = angular.module('sampleApp', []);
 
-//Define Routing for app
-//Uri /AddNewOrder -> template AddOrder.html and Controller AddOrderController
-//Uri /ShowOrders -> template ShowOrders.html and Controller AddOrderController
 sampleApp.config(['$routeProvider',
   function($routeProvider) {
-    $routeProvider.
-      when('/one', {
-	templateUrl: 'views/one.html',
-	controller: 'AddOrderController'
-      }).
-      when('/two', {
-	templateUrl: 'views/two.html',
-	controller: 'ShowOrdersController'
-      }).
-      otherwise({
-	redirectTo: '/one'
+    $routeProvider
+    .when('/map', {
+        templateUrl: 'views/mapview.html',
+        controller: 'MapCtrl'
+      })
+      .otherwise({
+          redirectTo: '/map'
       });
 }]);
 
-
-sampleApp.controller('AddOrderController', function($scope) {
-	
-	$scope.message = 'What up, I am the first view and it is a pleasure to make your aquaintance';
-	
+sampleApp.controller('OptCtrl', function($scope){
+    $scope.countries = getCountries(); 
 });
 
-
-sampleApp.controller('ShowOrdersController', function($scope) {
-
-	$scope.message = 'Hello there my dear chap, I am number two, what can i do for you';
-
+sampleApp.controller('MapCtrl', function($scope) {
+	$scope.message = 'Lets look at a map';
 });
-
